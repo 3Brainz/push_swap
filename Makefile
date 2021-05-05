d6227aed8c09ebdc2b31f47d0b31e4e978bacff3
Paolo roberto Punzo <ppunzo@e3r7p3.42roma.it>
2021-05-05 15:06:34 +0200

    mvp

--- Makefile
+++ Makefile
@@ -1,19 +1,23 @@
-CHECKER=./checker
-PUSH_SWAP=./push_swap
+CHECKER_DIR=./checker
+CHECKER=${CHECKER_DIR}/checker
+PUSH_SWAP_DIR=./push_swap
+PUSH_SWAP=${PUSH_SWAP_DIR}/push_swap
 MAKE=make -C
 MAKE_F=make fclean -C
 MAKE_C=make clean -C
+MOVE=mv ${CHECKER} ${PUSH_SWAP} .
+RM=rm -rf
 
 all:
-	${MAKE} ${CHECKER}
-	${MAKE} ${PUSH_SWAP}
+	${MAKE} ${CHECKER_DIR}
+	${MAKE} ${PUSH_SWAP_DIR}
 
 clean:
-	${MAKE_C} ${CHECKER}
-	${MAKE_C} ${PUSH_SWAP}
+	${MAKE_C} ${CHECKER_DIR}
+	${MAKE_C} ${PUSH_SWAP_DIR}
 
 fclean:
-	${MAKE_F} ${CHECKER}
-	${MAKE_F} ${PUSH_SWAP}
+	${MAKE_F} ${CHECKER_DIR}
+	${MAKE_F} ${PUSH_SWAP_DIR}
 
 re: fclean all
---
+++ push_swap/srcs/push_swap.c
@@ -0,0 +1,141 @@
+#include "push_swap.h"
+
+// long		min_num(t_numbers *stack)
+// {
+// 	int nu;
+
+// 	while ()
+// }
+
+int	ft_list_len(t_numbers *stack)
+{
+	int len;
+	t_numbers *curr_nu;
+
+	len = 0;
+	curr_nu = stack;
+	if (!curr_nu)
+		return (0);
+	while (curr_nu)
+	{
+		len += 1;
+		curr_nu = curr_nu->next;
+	}
+	return (len);
+}
+
+t_numbers	*min_node(t_numbers *stack)
+{
+	t_numbers	*ret;
+	t_numbers	*curr_nu;
+
+	ret = stack;
+	curr_nu = stack;
+	if (!curr_nu)
+		return (0);
+	while (curr_nu)
+	{
+		if (ret->number > curr_nu->number)
+		{
+			ret = curr_nu;
+		}
+		curr_nu = curr_nu->next;
+	}
+	return (ret);
+}
+
+t_numbers	*ft_succ_node(t_numbers *stack, t_numbers *rel_prev)
+{
+	t_numbers *ret;
+	t_numbers *curr_nu;
+
+	curr_nu = stack;
+	ret = 0;
+	if (!curr_nu || !rel_prev)
+		return (0);
+	while (curr_nu)
+	{
+		if (curr_nu->number > rel_prev->number)
+		{
+			if (ret)
+			{
+				if (curr_nu->number < ret->number)
+					ret = curr_nu;
+			}
+			else
+				ret = curr_nu;
+		}
+		curr_nu = curr_nu->next;
+	}
+	return (ret);
+}
+
+void	ft_indicize_list(t_numbers *stack)
+{
+	t_numbers	*min_nu;
+	int			index;
+
+	index = 0;
+	min_nu = min_node(stack);
+	min_nu->position = index;
+	while (ft_succ_node(stack, min_nu))
+	{
+		index += 1;
+		min_nu = ft_succ_node(stack, min_nu);
+		min_nu->position = index;
+	}
+}
+
+void	ft_sort_three(t_numbers *stack_a, t_numbers *stack_b)
+{
+	t_numbers	*curr_nu;
+	int			curr_elab;
+
+	curr_nu = stack_a;
+	curr_elab = ft_list_len(stack_a) - 1;
+	if (!curr_elab)
+		return ;
+	while (curr_nu)
+	{
+		
+	}
+}
+
+void	ft_case_analyzer(t_numbers *stack_a, t_numbers *stack_b)
+{
+	int	stack_len;
+
+	stack_len = ft_list_len(stack_a);
+	if (stack_len <= 3)
+	{
+
+	}
+	if (stack_len <= 5)
+	{
+
+	}
+	if (stack_len <= 100)
+	{
+
+	}
+	if (stack_len > 100)
+	{
+
+	}
+	
+}
+
+int main(int argc, char **argv)
+{
+	t_numbers	*stack_a;
+	t_numbers	*stack_b;
+
+	stack_a = 0;
+	stack_b = 0;
+	if (argc == 1)
+		exit(0);
+	ft_fill_stack_a(argv, &stack_a);
+	ft_indicize_list(stack_a);
+	ft_print_list(stack_a);
+	printf("len = %d\n", ft_list_len(stack_a));
+}
